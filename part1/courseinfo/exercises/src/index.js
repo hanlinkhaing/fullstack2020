8e968ddc2291f9e03538e95c06472391831bdabc
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-09-15 12:26:21 +0630

    course info step2

--- part1/courseinfo/exercises/src/index.js
+++ part1/courseinfo/exercises/src/index.js
@@ -10,6 +10,14 @@ const Header = (props) => (
 
 const Content = (props) => (
   <>
+    <Part part={props.part[0]} exercises={props.exercises[0]}/>
+    <Part part={props.part[1]} exercises={props.exercises[1]}/>
+    <Part part={props.part[2]} exercises={props.exercises[2]}/>
+  </>
+)
+
+const Part = (props) => (
+  <>
     <p>
       {props.part} {props.exercises}
     </p>
@@ -33,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
