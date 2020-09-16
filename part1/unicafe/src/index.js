cda24083ae4a6d48d3bb489edac360bb9605abcb
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-09-16 15:00:18 +0630

    unicafe final

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,28 +1,61 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const getTotal = () => good + neutral + bad;
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      {good || neutral || bad ? (
+        <table>
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={getTotal()} />
+            <Statistic text="average" value={(good - bad) / getTotal()} />
+            <Statistic text="positive" value={(good / getTotal()) * 100} />
+          </tbody>
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
+  );
+};
+
+const Button = ({ text, clickHandler }) => (
+  <button onClick={clickHandler}>{text}</button>
+);
+
+const Statistic = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>
+      {value}
+      {text === "positive" ? "%" : ""}
+    </td>
+  </tr>
+);
 const App = () => {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const addGood = () => setGood(good+1);
-  const addNeutral = () => setNeutral(neutral+1);
-  const addBad = () => setBad(bad+1);
+  const addGood = () => setGood(good + 1);
+  const addNeutral = () => setNeutral(neutral + 1);
+  const addBad = () => setBad(bad + 1);
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={addGood}>good</button>
-      <button onClick={addNeutral}>neutral</button>
-      <button onClick={addBad}>bad</button>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Button text="Good" clickHandler={addGood} />
+      <Button text="Netural" clickHandler={addNeutral} />
+      <Button text="Bad" clickHandler={addBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
