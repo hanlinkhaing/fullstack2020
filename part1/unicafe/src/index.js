915a18da1c7ba482a2ff3a6d6e7d1330082486dc
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-09-16 14:10:08 +0630

    unicafe step1

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGood = () => setGood(good+1);
+  const addNeutral = () => setNeutral(neutral+1);
+  const addBad = () => setBad(bad+1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={addGood}>good</button>
+      <button onClick={addNeutral}>neutral</button>
+      <button onClick={addBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
