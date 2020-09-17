57b71b4f3572e2e9aace3612b926ea544ce4fcb0
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-09-17 16:43:42 +0630

    phonebook

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      setPersons(res.data);
+    })
+  }
+
+  useEffect(hook, []);
 
   const changeNewName = (event) => {
     setNewName(event.target.value);
