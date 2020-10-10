ce0ead206fbfb9aa01a0f8e788fbb715074a2b66
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-10-10 14:40:57 +0630

    7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -20,10 +20,10 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {axios.get(baseUrl).then(res => setResources(res.data))}, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource).then(res => setResources(resources.concat(res.data)))
   }
 
   const service = {
