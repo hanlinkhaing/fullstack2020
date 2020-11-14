e2973698da2d867e63ed93539a709a6b68f4ece9
Han Lin Khaing <hanlinkhaing300695@gmail.com>
2020-11-14 14:08:02 +0630

    9.9

---
+++ part9/patientor_backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express(); // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors()); 
+app.use(express.json());
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
